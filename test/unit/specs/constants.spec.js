18ce34c89d6c1e15035edc0031d1f9cefd9d8b9a
Vikas Goyal <38314488+vikas-goyal-syncplicity@users.noreply.github.com>
2020-07-31 18:51:54 +0530

    test(ios): merged duplicate UT files

--- test/unit/specs/constants.spec.js
+++ test/unit/specs/constants.spec.js
@@ -1,28 +1,85 @@
-let BLE = require('appcelerator.ble');
+const BLE = require('appcelerator.ble');
 const IOS = (Ti.Platform.osname === 'iphone' || Ti.Platform.osname === 'ipad');
+
 describe('appcelerator.ble', function () {
-	it('can be required', () => {
-		expect(BLE).toBeDefined();
-	});
 	if (IOS) {
-		describe('constants', () => {
-			describe('AUTHORISATION_STATUS_*', () => {
-				it('AUTHORISATION_STATUS_NOT_DETERMINED', () => {
-					expect(BLE.AUTHORISATION_STATUS_NOT_DETERMINED).toEqual(jasmine.any(Number));
-				});
+		describe('AUTHORISATION_STATUS_*', () => {
+			it('AUTHORISATION_STATUS_NOT_DETERMINED', () => {
+				expect(BLE.AUTHORISATION_STATUS_NOT_DETERMINED).toEqual(jasmine.any(Number));
+			});
 
-				it('AUTHORISATION_STATUS_RESTRICTED', () => {
-					expect(BLE.AUTHORISATION_STATUS_RESTRICTED).toEqual(jasmine.any(Number));
-				});
+			it('AUTHORISATION_STATUS_RESTRICTED', () => {
+				expect(BLE.AUTHORISATION_STATUS_RESTRICTED).toEqual(jasmine.any(Number));
+			});
 
-				it('AUTHORISATION_STATUS_DENIED', () => {
-					expect(BLE.AUTHORISATION_STATUS_DENIED).toEqual(jasmine.any(Number));
-				});
+			it('AUTHORISATION_STATUS_DENIED', () => {
+				expect(BLE.AUTHORISATION_STATUS_DENIED).toEqual(jasmine.any(Number));
+			});
 
-				it('AUTHORISATION_STATUS_ALLOWED_ALWAYS', () => {
-					expect(BLE.AUTHORISATION_STATUS_ALLOWED_ALWAYS).toEqual(jasmine.any(Number));
-				});
+			it('AUTHORISATION_STATUS_ALLOWED_ALWAYS', () => {
+				expect(BLE.AUTHORISATION_STATUS_ALLOWED_ALWAYS).toEqual(jasmine.any(Number));
+			});
+		});
+
+		describe('ATTRIBUTE_PERMISSION_*', function () {
+			it('ATTRIBUTE_PERMISSION_READABLE', () => {
+				expect(BLE.ATTRIBUTE_PERMISSION_READABLE).toEqual(jasmine.any(Number));
+			});
+
+			it('ATTRIBUTE_PERMISSION_WRITEABLE', () => {
+				expect(BLE.ATTRIBUTE_PERMISSION_WRITEABLE).toEqual(jasmine.any(Number));
+			});
+
+			it('ATTRIBUTE_PERMISSION_READ_ENCRYPTION_REQUIRED', () => {
+				expect(BLE.ATTRIBUTE_PERMISSION_READ_ENCRYPTION_REQUIRED).toEqual(jasmine.any(Number));
+			});
+
+			it('ATTRIBUTE_PERMISSION_WRITE_ENCRYPTION_REQUIRED', () => {
+				expect(BLE.ATTRIBUTE_PERMISSION_WRITE_ENCRYPTION_REQUIRED).toEqual(jasmine.any(Number));
+			});
+		});
+
+		describe('CHARACTERISTIC_PROPERTIES_*', function () {
+			it('CHARACTERISTIC_PROPERTIES_BROADCAST', () => {
+				expect(BLE.CHARACTERISTIC_PROPERTIES_BROADCAST).toEqual(jasmine.any(Number));
+			});
+
+			it('CHARACTERISTIC_PROPERTIES_READ', () => {
+				expect(BLE.CHARACTERISTIC_PROPERTIES_READ).toEqual(jasmine.any(Number));
+			});
+
+			it('CHARACTERISTIC_PROPERTIES_WRITE_WITHOUT_RESPONSE', () => {
+				expect(BLE.CHARACTERISTIC_PROPERTIES_WRITE_WITHOUT_RESPONSE).toEqual(jasmine.any(Number));
+			});
+
+			it('CHARACTERISTIC_PROPERTIES_WRITE', () => {
+				expect(BLE.CHARACTERISTIC_PROPERTIES_WRITE).toEqual(jasmine.any(Number));
+			});
+
+			it('CHARACTERISTIC_PROPERTIES_NOTIFY', () => {
+				expect(BLE.CHARACTERISTIC_PROPERTIES_NOTIFY).toEqual(jasmine.any(Number));
+			});
+
+			it('CHARACTERISTIC_PROPERTIES_INDICATE', () => {
+				expect(BLE.CHARACTERISTIC_PROPERTIES_INDICATE).toEqual(jasmine.any(Number));
+			});
+
+			it('CHARACTERISTIC_PROPERTIES_AUTHENTICATED_SIGNED_WRITES', () => {
+				expect(BLE.CHARACTERISTIC_PROPERTIES_AUTHENTICATED_SIGNED_WRITES).toEqual(jasmine.any(Number));
+			});
+
+			it('CHARACTERISTIC_PROPERTIES_EXTENDED_PROPERTIES', () => {
+				expect(BLE.CHARACTERISTIC_PROPERTIES_EXTENDED_PROPERTIES).toEqual(jasmine.any(Number));
+			});
+
+			it('CHARACTERISTIC_PROPERTIES_NOTIFY_ENCRYPTION_REQUIRED', () => {
+				expect(BLE.CHARACTERISTIC_PROPERTIES_NOTIFY_ENCRYPTION_REQUIRED).toEqual(jasmine.any(Number));
+			});
+
+			it('CHARACTERISTIC_PROPERTIES_INDICATE_ENCRYPTION_REQUIRED', () => {
+				expect(BLE.CHARACTERISTIC_PROPERTIES_INDICATE_ENCRYPTION_REQUIRED).toEqual(jasmine.any(Number));
 			});
 		});
 	}
 });
+
