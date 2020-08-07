5702928c9dcfc29b705fc22f9e5fcd313240c889
Anil Shukla <40199172+anil-shukla-axway@users.noreply.github.com>
2020-08-07 12:51:53 +0530

    feat(iOS) Model for TiBLERequestProxy has been added

--- ios/Classes/TiBLERequestProxy.swift
+++ ios/Classes/TiBLERequestProxy.swift
@@ -6,7 +6,35 @@
 
 import Foundation
 import TitaniumKit
+import CoreBluetooth
 
 class TiBLERequestProxy: TiProxy {
-
+    private var _request: CBATTRequest
+    init(request: CBATTRequest) {
+        self._request = request
+    }
+    @objc
+    func central() -> TiBLECentralProxy {
+        return TiBLECentralProxy()
+    }
+    @objc
+    func characteristic() -> TiBLECharacteristicProxy {
+        return TiBLECharacteristicProxy(characteristic: _request.characteristic) as TiBLECharacteristicProxy
+    }
+    @objc
+    func offset() -> NSNumber {
+        return NSNumber(value: _request.offset)
+    }
+    @objc
+    func value() -> TiBuffer? {
+        guard let requestValue = _request.value else {
+            return nil
+        }
+        let buffer = TiBuffer()._init(withPageContext: self.pageContext)
+        buffer?.data = NSMutableData.init(data: requestValue)
+        return buffer
+    }
+    func request() -> CBATTRequest {
+        return _request
+    }
 }
