7fad99eff7714a754d22a6f449ceb8634133a7a2
akansha-maheshwari-axway <55788119+akansha-maheshwari-axway@users.noreply.github.com>
2021-01-14 16:19:15 +0530

    feat(android): manage requestProxy and centralProxy classes

--- android/src/appcelerator/ble/peripheral/TiBLERequestProxy.java
+++ android/src/appcelerator/ble/peripheral/TiBLERequestProxy.java
@@ -5,10 +5,74 @@
  */
 package appcelerator.ble.peripheral;
 
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothGattCharacteristic;
+import appcelerator.ble.TiBLECharacteristicProxy;
+import org.appcelerator.kroll.KrollDict;
 import org.appcelerator.kroll.KrollProxy;
 import org.appcelerator.kroll.annotations.Kroll;
+import org.appcelerator.kroll.common.Log;
+import ti.modules.titanium.BufferProxy;
 
 @Kroll.proxy
 public class TiBLERequestProxy extends KrollProxy
 {
+	private static final String LCAT = "TiBLERequestProxy";
+	private BluetoothGattCharacteristic characteristic;
+	private BluetoothDevice bluetoothDevice;
+	private int offset;
+	private BufferProxy value;
+
+	public TiBLERequestProxy(BluetoothGattCharacteristic characteristic, BluetoothDevice bluetoothDevice)
+	{
+		this.characteristic = characteristic;
+		this.bluetoothDevice = bluetoothDevice;
+	}
+
+	@Kroll.getProperty
+	public TiBLECharacteristicProxy characteristic()
+	{
+		return new TiBLECharacteristicProxy(characteristic);
+	}
+
+	@Kroll.getProperty
+	public TiBLECentralProxy central()
+	{
+		return new TiBLECentralProxy(bluetoothDevice);
+	}
+
+	@Kroll.getProperty
+	public int offset()
+	{
+		return offset;
+	}
+
+	@Kroll.getProperty
+	public BufferProxy value()
+	{
+		return value;
+	}
+
+	@Kroll.method
+	public void updateValue(KrollDict dict)
+	{
+		if (dict == null || !dict.containsKey("value")) {
+			Log.e(LCAT, "updateValue(): Cannot update value, required parameter not provided");
+			return;
+		}
+		value = (BufferProxy) dict.get("value");
+	}
+
+	public void setOffset(int offset)
+	{
+		this.offset = offset;
+	}
+
+	public void setValue(byte[] value)
+	{
+		if (value == null) {
+			this.value = null;
+		}
+		this.value = new BufferProxy(value);
+	}
 }
